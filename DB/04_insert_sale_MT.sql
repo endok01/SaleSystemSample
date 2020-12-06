e1330f118963b1e6d40e790c85bb7e85a21f7235
endo_k01 <endo_k01@B010191.pci-sol.local>
2020-12-06 22:18:42 +0900

    初期データ修正（日付別集計対応）

--- DB/04_insert_sale_MT.sql
+++ DB/04_insert_sale_MT.sql
@@ -5,14 +5,14 @@ insert into MT_ITEM_GENRE values('00000002','クリアファイル');
 insert into MT_ITEM_GENRE values('00000003','付箋');
 
 
-insert into MT_ITEM values('00000001', 'コピー用紙A4_01',    '00000001', 'A4',          3000);
-insert into MT_ITEM values('00000002', 'コピー用紙A3_01',    '00000001', 'A3',          3100);
-insert into MT_ITEM values('00000003', 'コピー用紙B5_01',    '00000001', 'B5',          2900);
-insert into MT_ITEM values('00000004', 'クリアファイル_010', '00000002', '10枚パック',   100);
-insert into MT_ITEM values('00000005', 'クリアファイル_100', '00000002', '100枚パック', 1000);
-insert into MT_ITEM values('00000006', '付箋_大_010',        '00000003', '大',           900);
-insert into MT_ITEM values('00000007', '付箋_中_010',        '00000003', '中',           700);
-insert into MT_ITEM values('00000008', '付箋_小_010',        '00000003', '小',           600);
+insert into MT_ITEM values('CP000001', 'コピー用紙A4 01',       '00000001', 'A4',          3000);
+insert into MT_ITEM values('CP000002', 'コピー用紙A3 01',       '00000001', 'A3',          3100);
+insert into MT_ITEM values('CP000003', 'コピー用紙B5 01',       '00000001', 'B5',          2900);
+insert into MT_ITEM values('CF000001', 'クリアファイル 10枚P',  '00000002', '10枚パック',   100);
+insert into MT_ITEM values('CF000002', 'クリアファイル 100枚P', '00000002', '100枚パック',  900);
+insert into MT_ITEM values('SN000001', '付箋紙 大 10P',         '00000003', '大',           700);
+insert into MT_ITEM values('SN000002', '付箋紙 中 10P',         '00000003', '中',           500);
+insert into MT_ITEM values('SN000003', '付箋紙 小 10P',         '00000003', '小',           300);
 
 insert into MT_CUSTOMER values('C0000001', '新橋商店');
 insert into MT_CUSTOMER values('C0000002', '虎ノ門商事');
